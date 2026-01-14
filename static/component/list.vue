be481aa92f5fb6799e67c0e35520f8b9484389fb
appl <appl@wimift.com>
2026-01-14 15:41:32 +0800

    update

---
+++ static/component/list.vue
@@ -0,0 +1,30 @@
+<style scoped>
+#menu-list-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
+
+<template>
+    <div>
+        <h2 style="text-align: center; margin-top: 100px">
+            hello world
+        </h2>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+        }
+    },
+    mounted() {
+    },
+    beforeDestroy() {
+    },
+    methods: {
+    }
+}
+</script>
