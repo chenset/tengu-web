74165bef74a3092f59ed6bf2a4985d2d8ca45717
appl <appl@wimift.com>
2026-02-03 16:40:31 +0800

    update

--- static/component/list.vue
+++ static/component/list.vue
@@ -371,7 +371,7 @@
                         创建内网实例
                     </button>
                     <button @click="openCreateWanDialog"
-                        v-if="currentLoginAccount?.permissionList.includes('R_WAN')"
+                        v-if="currentLoginAccount?.permissionList?.includes('R_WAN')"
                         class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-500 hover:bg-orange-600 disabled:bg-orange-300 disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
                         创建公网实例
                     </button>
