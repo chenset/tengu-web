a142b9cd95a154019adb775525c16c52d3a57e6b
appl <appl@wimift.com>
2026-02-04 12:12:18 +0800

    update

--- static/component/user.vue
+++ static/component/user.vue
@@ -19,7 +19,7 @@
                     <label class="block text-sm font-medium text-gray-700 mb-2">
                         昵称
                     </label>
-                    <input type="text" v-model="createForm.nickname" placeholder="可选，不填则使用邮箱"
+                    <input type="text" v-model="createForm.nickname" placeholder="可选，不填则从邮箱提取"
                         class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 </div>
 
