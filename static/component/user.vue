52423e7f63b3a97d422135810d6ea155ec2c9ac2
appl <appl@wimift.com>
2026-02-08 22:39:37 +0800

    update

--- static/component/user.vue
+++ static/component/user.vue
@@ -19,7 +19,7 @@
                     <label class="block text-sm font-medium text-gray-700 mb-2">
                         昵称
                     </label>
-                    <input type="text" v-model="createForm.nickname" placeholder="请输入飞书中对应的用户真名，用于发送飞书消息提醒"
+                    <input type="text" v-model="createForm.nickname" placeholder="请输入飞书中的用户真名，用于飞书消息提醒"
                         class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 </div>
 
